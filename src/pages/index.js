4a3ad50189c361367d802f6cd1aafca69d3e53d9
Charles Pine <charlespine22@gmail.com>
2023-06-01 05:27:53 -0500

    Categories wrapper added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import { client } from '../../lib/client';
+import React, { useState } from "react";
+import { client } from "../../lib/client";
 import {
   Product,
   FooterBanner,
   HeroBanner,
   CategoryBanner,
-} from '../../components';
+} from "../../components";
 
 const Home = ({ products, bannerData, categoryData }) => {
-  const [productsFilter, setProductsFilter] = useState('TOP');
+  const [productsFilter, setProductsFilter] = useState("TOP");
   const newReleases = products?.slice(-5);
   const topSellers = products?.slice(0, 5);
   categoryData.sort((a, b) => a.title.localeCompare(b.title)).reverse();
@@ -16,7 +16,7 @@ const Home = ({ products, bannerData, categoryData }) => {
   return (
     <>
       <HeroBanner heroBanner={bannerData.length && bannerData[1]} />
-      <div className='quote-container'>
+      <div className="quote-container">
         <p>
           Style is the only thing you can&#39;t buy. It&#39;s not in a shopping
           bag, a label, or a price tag. It&#39;s something reflected from our
@@ -24,50 +24,53 @@ const Home = ({ products, bannerData, categoryData }) => {
         </p>
         <span>Alber Elbaz</span>
       </div>
-      <div className='categories-container'>
-        {categoryData.length &&
-          categoryData.map((category, i) => (
-            <CategoryBanner
-              key={category._id}
-              categoryBanner={category}
-              index={i}
-            />
-          ))}
+      <div className="categories-wrapper">
+        <h3>Explore</h3>
+        <div className="categories-container">
+          {categoryData.length &&
+            categoryData.map((category, i) => (
+              <CategoryBanner
+                key={category._id}
+                categoryBanner={category}
+                index={i}
+              />
+            ))}
+        </div>
       </div>
-      <div className='products-heading'>
-        <div className='products-filters'>
+      <div className="products-heading">
+        <div className="products-filters">
           <button
             style={{
-              borderBottom: productsFilter === 'TOP' && '1px solid black',
-              fontWeight: productsFilter === 'TOP' && '700',
+              borderBottom: productsFilter === "TOP" && "1px solid black",
+              fontWeight: productsFilter === "TOP" && "700",
             }}
-            onClick={() => setProductsFilter('TOP')}
+            onClick={() => setProductsFilter("TOP")}
           >
             Top Sellers
           </button>
           <button
             styles={{
-              borderBottom: productsFilter === 'NEW' && '1px solid black',
-              fontWeight: productsFilter === 'NEW' && '700',
+              borderBottom: productsFilter === "NEW" && "1px solid black",
+              fontWeight: productsFilter === "NEW" && "700",
             }}
-            onClick={() => setProductsFilter('NEW')}
+            onClick={() => setProductsFilter("NEW")}
           >
             New Releases
           </button>
         </div>
         <h2>
-          {productsFilter === 'TOP' ? 'Top Selling Products' : 'New Releases'}
+          {productsFilter === "TOP" ? "Top Selling Products" : "New Releases"}
         </h2>
         <p>So much variety, you can&#39;t not find something for you!</p>
       </div>
-      <div className='products-container'>
-        {productsFilter === 'TOP'
+      <div className="products-container">
+        {productsFilter === "TOP"
           ? topSellers.map((product) => (
               <Product key={product._id} product={product} />
             ))
           : newReleases.map((product) => (
               <Product key={product._id} product={product} />
-            ))}{' '}
+            ))}{" "}
       </div>
       <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
