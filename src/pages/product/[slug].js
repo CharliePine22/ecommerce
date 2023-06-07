34d68e94985cf71e93006cd47d1bc51c9f3dadeb
Charles Pine <charlespine22@gmail.com>
2023-06-07 17:03:26 -0500

    Added try/catch to static paths to catch null

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import { client, urlFor } from '../../../lib/client';
+import React, { useState } from "react";
+import { client, urlFor } from "../../../lib/client";
 import {
   AiOutlineMinus,
   AiOutlinePlus,
   AiFillStar,
   AiOutlineStar,
-} from 'react-icons/ai';
-import { Product } from '../../../components';
-import { useStateContext } from '../../../context/StateContext';
+} from "react-icons/ai";
+import { Product } from "../../../components";
+import { useStateContext } from "../../../context/StateContext";
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
@@ -23,80 +23,80 @@ const ProductDetails = ({ product, products }) => {
 
   return (
     <div>
-      <div className='product-detail-container'>
+      <div className="product-detail-container">
         <div>
-          <div className='image-container'>
+          <div className="image-container">
             <img
               src={urlFor(image && image[index])}
-              className='product-detail-image'
+              className="product-detail-image"
               style={{
-                objectFit: image[index].asset._ref.slice(-3) == 'jpg' && 'fill',
+                objectFit: image[index].asset._ref.slice(-3) == "jpg" && "fill",
               }}
             />
           </div>
-          <div className='small-images-container'>
+          <div className="small-images-container">
             {image?.map((item, i) => (
               <img
                 src={urlFor(item)}
                 key={item._key}
                 className={
-                  i === index ? 'small-image selected-image' : 'small-image'
+                  i === index ? "small-image selected-image" : "small-image"
                 }
                 onMouseEnter={() => setIndex(i)}
                 style={{
                   objectFit:
-                    image[index].asset._ref.slice(-3) == 'jpeg' && 'cover',
+                    image[index].asset._ref.slice(-3) == "jpeg" && "cover",
                 }}
               />
             ))}
           </div>
         </div>
-        <div className='product-detail-desc'>
+        <div className="product-detail-desc">
           <h1>{name}</h1>
-          <div className='reviews'>
-            <div style={{ marginTop: '5px' }}>
+          <div className="reviews">
+            <div style={{ marginTop: "5px" }}>
               <AiFillStar />
               <AiFillStar />
               <AiFillStar />
               <AiFillStar />
-              <AiOutlineStar color={'black'} />
+              <AiOutlineStar color={"black"} />
             </div>
             <p>(20)</p>
           </div>
           <h4>Details:</h4>
           <p>{details}</p>
-          <p className='price'>${price}</p>
-          <div className='quantity'>
+          <p className="price">${price}</p>
+          <div className="quantity">
             <h3>Quantity</h3>
-            <p className='quantity-desc'>
-              <span className='minus' onClick={() => decreaseQuantity()}>
+            <p className="quantity-desc">
+              <span className="minus" onClick={() => decreaseQuantity()}>
                 <AiOutlineMinus />
               </span>
-              <span className='num'>{quantity}</span>
-              <span className='plus' onClick={() => increaseQuantity()}>
+              <span className="num">{quantity}</span>
+              <span className="plus" onClick={() => increaseQuantity()}>
                 <AiOutlinePlus />
               </span>
             </p>
           </div>
-          <div className='buttons'>
+          <div className="buttons">
             <button
-              type='button'
-              className='add-to-cart'
+              type="button"
+              className="add-to-cart"
               onClick={() => onAdd(product, quantity)}
             >
               Add to Cart
             </button>
-            <button type='button' className='buy-now' onClick={handleBuyNow}>
+            <button type="button" className="buy-now" onClick={handleBuyNow}>
               Buy Now
             </button>
           </div>
         </div>
       </div>
 
-      <div className='maylike-products-wrapper'>
+      <div className="maylike-products-wrapper">
         <h2>You may also like</h2>
-        <div className='marquee'>
-          <div className='maylike-products-container track'>
+        <div className="marquee">
+          <div className="maylike-products-container track">
             {products.map((item) => (
               <Product key={item._id} product={item} />
             ))}
@@ -115,21 +115,30 @@ export const getStaticPaths = async () => {
   }`;
 
   const products = await client.fetch(query);
-  console.log(products);
-  const paths = products.map((product) => ({
-    params: {
-      slug: product.slug.current,
-    },
-  }));
+  console.log(products.length);
+  try {
+    const paths = products.map((product) => ({
+      params: {
+        slug: product.slug.current,
+      },
+    }));
 
-  return {
-    paths,
-    fallback: 'blocking',
-  };
+    return {
+      paths,
+      fallback: "blocking",
+    };
+  } catch (e) {
+    console.log(e);
+    return {
+      paths: [],
+      fallback: "blocking",
+    };
+  }
 };
 
 export const getStaticProps = async ({ params: { slug } }) => {
   const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+  console.log("FETCHING PRODUCT");
   const productsQuery = '*[_type == "product"]';
   const product = await client.fetch(query);
   console.log(product);
